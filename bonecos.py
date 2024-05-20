eaab97a8dfae8119870928dd3afc8379209878cc
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-20 01:18:43 -0300

    criado lista de bonecos

---
+++ bonecos.py
@@ -0,0 +1,54 @@
+# boneco[0] - inicial
+# boneco[1] - 1 erro
+# boneco[2] - 2 erros
+# boneco[3] - 3 erros
+# boneco[4] - 4 erros
+# boneco[5] - 5 erros
+# boneco[6] - 6 erros
+
+
+# Definindo a string do boneco
+imagem = [
+"""
+===|
+|
+|
+|
+""",
+"""
+===|
+|  0
+|
+|
+""",
+"""
+===|
+| \\0
+|
+|
+""",
+"""
+===|
+| \\0/
+|
+|
+""",
+"""
+===|
+| \\0/
+|  |
+|
+""",
+"""
+===|
+| \\0/
+|  |
+| /
+""",
+"""
+===|
+| \\0/
+|  |
+| / \\
+""",
+]
